8b471238679495e33c36f8f3e895014b55eab314
Shetkar Harshitha Patil <shetkarharsha49@gmail.com>
2026-09-11 13:38:03 +0530

    Add BigQuery aggregation query

--- sql/bigquery_aggregation.sql
+++ sql/bigquery_aggregation.sql
@@ -1,1 +1,17 @@
 
+INSERT INTO `snappy-mapper-498509-e0.ecom_analytics2.daily_product_summary` (
+    product_id,
+    total_items_sold,
+    total_orders,
+    aggregated_at
+)
+SELECT
+    product_id,
+    SUM(quantity) AS total_items_sold,
+    COUNT(DISTINCT order_id) AS total_orders,
+    CURRENT_TIMESTAMP() AS aggregated_at
+FROM EXTERNAL_QUERY(
+    "projects/snappy-mapper-498509-e0/locations/asia-south1/connections/spanner-ecom-conn",
+    "SELECT order_id, product_id, quantity FROM EcomOrders"
+)
+GROUP BY product_id;
