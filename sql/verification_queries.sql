0026d91671cd3b300bebcf83fe395288756f43d8
Shetkar Harshitha Patil <shetkarharsha49@gmail.com>
2026-09-11 13:43:50 +0530

    Add verification SQL structure

--- sql/verification_queries.sql
+++ sql/verification_queries.sql
@@ -1,1 +1,10 @@
 
+SELECT  
+    product_id, 
+    total_items_sold, 
+    total_orders, 
+    aggregated_at 
+FROM `snappy-mapper-498509-e0.ecom_analytics.daily_product_summary` 
+ORDER BY total_items_sold DESC;
+
+ 
